a85ee407c879b33842998126a9da5d25f496bc02
scache <scal.che@gmail.com>
2019-02-23 11:43:28 +0900

    values by reflection

--- src/test/kotlin/MySealedTest.kt
+++ src/test/kotlin/MySealedTest.kt
@@ -1,27 +1,42 @@
-import org.hamcrest.CoreMatchers.`is`
-import org.junit.Assert.assertThat
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import kotlin.reflect.full.isSubclassOf
 
 class MySealedTest {
-  @Test fun valuesNotNulls() {
-    assertThat(MySealed.values.none { it == null }, `is`(true))
-  }
+//  @Test fun valuesNotNulls() {
+//    assertThat(MySealed.values.none { it == null }, `is`(true))
+//  }
 
-  // valuesNotNulls2 だけ実行すると valuesで返却されるリストでMySealed.Bに該当するものがnullになる
-  @Test fun valuesNotNulls2() {
-    System.out.println(MySealed.B)
-    System.out.println(MySealed.values.joinToString())
-    assertTrue(MySealed.values.none { it == null })
-  }
+    // valuesNotNulls2 だけ実行すると valuesで返却されるリストでMySealed.Bに該当するものがnullになる
+    @Test
+    fun valuesNotNulls2() {
+        System.out.println(MySealed.B)
+        System.out.println(MySealed.values.joinToString())
+        assertTrue(MySealed.values.none { it == null })
+    }
 
-  @Test fun valuesNotNulls3() {
-    assertTrue(mySealedValues.none { it == null })
-  }
+    @Test
+    fun valuesNotNulls3() {
+        assertTrue(mySealedValues.none { it == null })
+    }
 
-  @Test fun valuesNotNulls4() {
-    System.out.println(MySealed.B)
-    System.out.println(mySealedValues.joinToString())
-    assertTrue(mySealedValues.none { it == null })
-  }
+    @Test
+    fun valuesNotNulls4() {
+        System.out.println(MySealed.B)
+        System.out.println(mySealedValues.joinToString())
+        assertTrue(mySealedValues.none { it == null })
+    }
+
+    @Test
+    fun valuesByReflection() {
+        assertTrue(
+                MySealed::class.nestedClasses
+                        .filter { it.isFinal && it.isSubclassOf(MySealed::class) }
+                        .map { it.objectInstance as MySealed }
+                        .onEach { println(it) }
+                        .none { it == null }
+        )
+
+
+    }
 }
